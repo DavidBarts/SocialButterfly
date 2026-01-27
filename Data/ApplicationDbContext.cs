efe4a54307e037bd756b91b64d945b71b8252b4a
David Barts <david.w.barts@gmail.com>
2026-01-27 11:04:32 -0800

    Freshly-scaffolded auth-enabled site.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthTest.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
