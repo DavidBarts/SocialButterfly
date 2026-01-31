ae9a7796b2be31a090fbf74f8adcf6d069ba6a84
David Barts <david.w.barts@gmail.com>
2026-01-30 21:43:17 -0800

    More cleanups; mostly need to work on the auth pages next.

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SocialButterfly.Pages;
+
+public class AboutModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
