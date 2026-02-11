3a1946b5852967b1ad101e5d5580f4228d008d9e
David Barts <david.w.barts@gmail.com>
2026-02-11 11:33:32 -0800

    Fix email sending bug. Test of using pages not performed yet.

--- Lib/SmtpEmailSender.cs
+++ Lib/SmtpEmailSender.cs
@@ -4,11 +4,12 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace SocialButterfly.Lib;
 
-public class SmtpEmailSender(IConfiguration config, Secret secret) : IEmailSender
+public class SmtpEmailSender(IConfiguration config, Secret secret, ILogger<SmtpEmailSender> logger) : IEmailSender
 {
-    private IConfiguration config = config.GetRequiredSection("SMTP");
-    private IConfiguration secret = secret.Config.GetRequiredSection("SMTP");
-    
+    private readonly IConfiguration config = config.GetRequiredSection("SMTP");
+    private readonly IConfiguration secret = secret.Config.GetRequiredSection("SMTP");
+    private readonly ILogger<SmtpEmailSender> logger = logger;
+
     public async Task SendEmailAsync(string recipient, string subject, string message)
     {
         var mailMessage = new MailMessage(
@@ -26,6 +27,14 @@ public class SmtpEmailSender(IConfiguration config, Secret secret) : IEmailSende
         smtpClient.Credentials = new NetworkCredential(
             secret.GetRequiredValue<string>("Username"),
             secret.GetRequiredValue<string>("Password"));
+        using var semaphore = new SemaphoreSlim(0);
+        smtpClient.SendCompleted += new SendCompletedEventHandler((_, e) => {
+            semaphore.Release();
+            if (e.Error != null) {
+                logger.LogError("Sending mail to {recipient} failed: {error}", recipient, e.Error.ToString());
+            }
+        });
         smtpClient.SendAsync(mailMessage, null);
+        await semaphore.WaitAsync();
     }
 }
